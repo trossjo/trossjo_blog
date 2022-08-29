263c4f11cd63292df989c9cf6df3f20eb778c230
trossjo <39522225+trossjo@users.noreply.github.com>
2022-08-29 15:38:24 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,6 +1,6 @@
-module.exports = {https://github.com/trossjo/trossjo_blog/blob/master/gatsby-meta-config.js
-  title: `trossjo.com`,
-  description: `트로스조`,
+module.exports = {
+  title: `zoomkoding.com`,
+  description: `줌코딩의 개발일기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
   siteUrl: `https://www.zoomkoding.com`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
@@ -11,16 +11,16 @@ module.exports = {https://github.com/trossjo/trossjo_blog/blob/master/gatsby-met
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `조을한`,
+    name: `정진혁`,
     bio: {
       role: `개발자`,
-      description: ['능동', '효율'],
+      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/trossjo`,
+      github: ``, // `https://github.com/zoomKoding`,
       linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `trossjo@gmail.com`,
+      email: ``, // `zoomkoding@gmail.com`,
     },
   },
 
@@ -43,7 +43,7 @@ module.exports = {https://github.com/trossjo/trossjo_blog/blob/master/gatsby-met
       // ========================================================
       // ========================================================
       {
-        date: '2022.08 ~',
+        date: '2021.02 ~',
         activity: '개인 블로그 개발 및 운영',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
@@ -72,10 +72,10 @@ module.exports = {https://github.com/trossjo/trossjo_blog/blob/master/gatsby-met
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그',
+        title: '개발 블로그 테마 개발',
         description:
-          '개발 블로그 테마 연습',
-        techStack: ['python', 'php' , 'Codeinighter', 'Django'],
+          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
+        techStack: ['gatsby', 'react'],
         thumbnailUrl: 'blog.png',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
