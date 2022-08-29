17aa3f832eb8e6b286f396193a637e35f4bd2e06
trossjo <39522225+trossjo@users.noreply.github.com>
2022-08-29 15:58:14 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -72,10 +72,10 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '개발일지',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
+          '개발일지 테스트',
+        techStack: ['PYTHON', 'PHP', 'codeignighter', 'django'],
         thumbnailUrl: 'blog.png',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
